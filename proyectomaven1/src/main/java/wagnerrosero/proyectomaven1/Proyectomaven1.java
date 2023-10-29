a1be1549db1edb341f32b5160378d45238ad0251
wagnerroserom <wrosero9059@utm.edu.ec>
2023-10-28 19:20:06 -0500

    Se emplea el objeto secure random para crear las positiveFeedback

---
+++ proyectomaven1/src/main/java/wagnerrosero/proyectomaven1/Proyectomaven1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package wagnerrosero.proyectomaven1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Proyectomaven1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
